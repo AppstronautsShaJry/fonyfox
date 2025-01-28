cda5fbaa407a95e2d0b88c0e7f5081b29ae15a38
Jerry <azhagirishankar@appstronauts.in>
2025-01-28 15:40:15 +0530

    #first commit

---
+++ common/config/params.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'bsVersion' => '3.1',
+    'bsDependencyEnabled' => false,
+    'adminEmail' => 'admin@yourdomain.co',
+    'supportEmail' => 'support@yourdomain.co',
+    'senderEmail' => 'admin@yourdomain.co',
+    'senderName' => 'SayHi',
+    'siteMode' => 1,
+    'siteUrl' => 'https://example.com',
+    'siteName'=>'SayHi',
+    'enventoPurchaseCode' => '##########',
+    'releaseVersion' => '1.5',
+    'testOtp' => '111111',
+    'apiKey.firebaseCloudMessaging'=> '#################',
+    'user.passwordResetTokenExpire' => 3600,
+];
